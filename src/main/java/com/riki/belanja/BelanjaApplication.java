c3b216583ea3aff5a52f606997af9a4b82f0f232
n0tx <rcandra91@msn.com>
2024-08-25 00:26:26 +0700

    Spring Initializer: Api Web, Mysql, JPA, Lombok

---
+++ src/main/java/com/riki/belanja/BelanjaApplication.java
@@ -0,0 +1,13 @@
+package com.riki.belanja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelanjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelanjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riki/belanja/BelanjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riki.belanja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelanjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
